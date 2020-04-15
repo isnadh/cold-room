f32b20d911d285faeb66c9fa5963d5b1ffef0ca8
luisefaknak <luisefaknak@gmail.com>
2020-04-15 08:52:40 +0700

    add file crc P'oh and implement

--- crc esp8266/worknakrub/Crc16_test/Crc16_test234/Crc16_test234.ino
+++ crc esp8266/worknakrub/Crc16_test/Crc16_test234/Crc16_test234.ino
@@ -21,18 +21,19 @@ void toggle_led(){
 
 void loop(){
   crc.clearCrc();
-  byte data[] = " \"Data\": {\"temp_1\": 25.5, \"temp_2\": 24.4, \"temp_3\": \"N/A\",\"temp_4\": \"N/A\" } ";
-  char datana[] = " \"Data\": {\"temp_1\": 25.5, \"temp_2\": 24.4, \"temp_3\": \"N/A\",\"temp_4\": \"N/A\" } ";
+  uint8_t data[] = " \"Data\": {\"temp_1\": 25.5, \"temp_2\": 24.4, \"temp_3\": \"N/A\",\"temp_4\": \"N/A\" } ";
+  char data1[] = " \"Data\": {\"temp_1\": 25.5, \"temp_2\": 24.4, \"temp_3\": \"N/A\",\"temp_4\": \"N/A\" } ";
+  
   delay(1000);
   ///cheack array size //////
   Serial.println("--------------------------");
-  uint8_t sizeofdata = sizeof(data)/sizeof(data[0]);
+  uint8_t sizeofdata = sizeof(data);
   sprintf(buffer1,"sizeofdata == %d",sizeofdata);
   Serial.println(buffer1);
   Serial.println("--------------------------");
 
-  for(int i=0;i <=sizeofdata;i++){
-      Serial.print(datana[i]);
+  for(int i=0;i < (sizeof(data) -1);i++){
+      Serial.print(data1[i]);
       crc.updateCrc(data[i]);
   }
   /////////crc total
@@ -41,14 +42,14 @@ void loop(){
   Serial.println(buffer1);
   
   //Modbus
-  value = crc.Modbus(data,0,sizeofdata);
+  value = crc.Modbus(data,0,sizeof(data));
   sprintf(buffer1," MODBUS crc = 0x%x",value);
   Serial.println(buffer1);
  
   memset(data, 0, sizeof(data));
   
   //data clear array ////
-  for(int i=0;i <= sizeofdata;i++){
+  for(int i=0;i < (sizeof(data) -1);i++){
      sprintf(buffer1, "array data %d",data[i]);
      Serial.println(buffer1);
   }
